70905b6e7443e8255fe382367bec4f93d65a9f74
smell-of-curry <75345244+smell-of-curry@users.noreply.github.com>
2026-06-16 21:00:12 -0400

    Add profiling

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 
 import (
 	"log/slog"
+	"net/http"
+	_ "net/http/pprof" // registers /debug/pprof handlers on http.DefaultServeMux
 	"time"
 
 	"github.com/df-mc/dragonfly/server/player/chat"
@@ -11,11 +13,30 @@ import (
 	"github.com/smell-of-curry/pokebedrock-hub/pokebedrock"
 )
 
+// pprofAddress is the localhost-only address the debug/pprof server listens on.
+// It is bound to loopback so profiles are only reachable on the host (use an
+// SSH tunnel, e.g. `ssh -L 6060:localhost:6060 <host>`, to read them remotely).
+const pprofAddress = "localhost:6060"
+
 // init sets up basic logging until config is loaded
 func init() {
 	// Default to debug during startup, will be reconfigured after config load
 	slog.SetLogLoggerLevel(slog.LevelDebug)
 	chat.Global.Subscribe(chat.StdoutSubscriber{})
+}
+
+// startPprof starts the localhost-only pprof HTTP server used to capture
+// goroutine and heap profiles while the hub is running. It runs until the
+// process exits.
+//
+// @param log The logger used to report the listen address and any fatal error.
+func startPprof(log *slog.Logger) {
+	go func() {
+		log.Info("pprof debug server listening", "addr", pprofAddress)
+		if err := http.ListenAndServe(pprofAddress, nil); err != nil {
+			log.Error("pprof debug server stopped", "error", err)
+		}
+	}()
 }
 
 // main is the entry point for the application. It initializes the configuration,
@@ -50,6 +71,8 @@ func main() {
 
 	slog.SetLogLoggerLevel(logLevel)
 	log.Info("Log level set", "level", logLevel.String())
+
+	startPprof(log)
 
 	poke, err := pokebedrock.NewPokeBedrock(log, conf)
 	if err != nil {
